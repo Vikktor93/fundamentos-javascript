caf308eb0afebfb7de499b9a3c9a7355df08cef7
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-09-09 22:19:54 -0300

    Se actualizan los comentarios del fichero

--- JS/003.variables.js
+++ JS/003.variables.js
@@ -1,32 +1,35 @@
-/*GUIA RÁPIDA: VARIABLES EN JS -  30 DE JUNIO 2023
-Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos*/
+// GUIA CURSO INTRODUCTORIO JS - 09 DE SEPTIEMBRE 2024
+// Docente: Victor Saldivia Vera
+// Universidad de Los Lagos
 
-//Variables que pueden cambiar con el tiempo
+
+// Variables que pueden cambiar con el tiempo
 var estatura = 1.71
 let peso = 60
 
-//Constante que no pueden cambiar
+// Constante que no pueden cambiar
 const nombre = "Victor"
 
-//OTROS EJEMPLOS
-//Declarando las Variables
+// OTROS EJEMPLOS
+// Declarando las Variables
 var edad
 let ciudad
 
-//Inicializando las Variables
+// Inicializando las Variables
 edad = 29
 ciudad = "Osorno"
 
-//Declaración e Inicialización de una Variable
+// Declaración e Inicialización de una Variable
 const apellido = "Saldivia"
 
 
-//Concatenando Variables de tipo String
+// Concatenando Variables de tipo String
 const nombrecompleto = nombre + " " + apellido
 console.log(nombrecompleto);
 
 
-//Declarando e Inicializando Variables en una Línea
+// Declarando e Inicializando Variables en una Línea
+// Uso Incorrecto de Declaración de Variables
 let docente = 'Tomas', age = 25, asignaturas = ['IA', 'Seguridad Informática'];
 
 
