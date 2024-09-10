59fe182e4ee63196c128e5f7da3d2e69f3be0fac
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-09-09 22:40:45 -0300

    Se modifica comentarios del archivo

---
+++ JS/005.prompt-confirm.js
@@ -0,0 +1,12 @@
+// GUIA CURSO INTRODUCTORIO JS - 09 DE SEPTIEMBRE 2024
+// Docente: Victor Saldivia Vera
+// Universidad de Los Lagos
+
+// Utilizando el Prompt
+let edad = prompt('¿Cuál es tu edad?', ''); // Contiene un texto a mostrar al usuario y un valor inicial (opcional)
+alert(`Tienes ${edad} años!`); // El Valor que ingreses*/
+
+
+// Utilizando el Confirm
+let question = confirm("¿Pasarás el ramo?"); // Modal con una pregunta y 2 botones
+alert(question) // Si se pulsa Aceptar lanzará True, si se pulsa Cancelar lanzará False*/
