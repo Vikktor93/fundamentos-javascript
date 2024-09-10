274e876dce4b5811d4ec5eda893f2d1de8054c0f
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-09-09 22:14:45 -0300

    Se agregan comentarios al fichero

---
+++ JS/001.holamundo.js
@@ -0,0 +1,8 @@
+// GUIA CURSO INTRODUCTORIO JS - 28 DE AGOSTO 2024
+// Docente: Victor Saldivia Vera
+// Universidad de Los Lagos
+
+/* Este es un Comentario */
+alert("Hola Mundo! - JS Externo");
+console.log("Hola Mundo!");
+
