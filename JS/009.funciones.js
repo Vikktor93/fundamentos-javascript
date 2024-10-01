0b967ba73b89a3fed80bf1cc8eb84082635a1ab8
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-10-01 19:47:40 -0300

    Modificación fichero de funciones

--- JS/009.funciones.js
+++ JS/009.funciones.js
@@ -1,29 +1,36 @@
-/*GUIA RÁPIDA: CONDICIONALES EN JS -  03 de Octubre 2023
-Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos*/
+// GUIA CURSO INTRODUCTORIO JS - 01 DE OCTUBRE   2024
+// Docente: Victor Saldivia Vera
+// Universidad de Los Lagos
 
-console.log('\n################ FUNCIÓN BÁSICA ################')
+console.log('\n################ FUNCIÓN BÁSICA (FUNCIÓN POR DECLARACIÓN) ################')
 function mostrarMensaje() {
-    alert('¡Hola a todos!');
+    console.log('¡Hola a todos!');
 }
 
 //Llamando a la función
 mostrarMensaje();
 
+console.log('\n################ FUNCIONES POR EXPRESIONES ################')
+const saludo = function saludar() {
+    return "Hola";
+}
+
+saludo(); //Hola
 
 console.log('\n################ VARIABLES INTERNAS ################')
 function varInterna() {
     let mensaje = "Hola, ¡Soy un Mensaje de una Variable Local!"; // variable local
-    alert(mensaje);
+    console.log(mensaje);
 }
 varInterna(); // Hola, ¡Soy un Mensaje de una Variable Local!
-//alert(mensaje); // ¿¡Error! La variable es local para esta función
+alert(mensaje); // ¿¡Error! La variable es local para esta función
 
 
 console.log('\n################ VARIABLES EXTERNAS ################')
 let nombre = 'Victor'; //variable global
 function mostrarNombre() {
-    let msj = '¡Hola!, ' + nombre;
-    alert(msj);
+    let msj = '¡Hola!, ' + 'mi nombre es ' + nombre;
+    console.log(msj);
 }
 
 mostrarNombre(); // ¡Hola!, Victor
@@ -33,7 +40,7 @@ let institucion = 'Ulagos'; //variable global
 function mostrarInstitucion() {
     let institucion = "Universidad de Los Lagos";
     let texto = 'Yo estudio en la ' + institucion;
-    alert(texto);
+    console.log(texto);
 }
 
 //La función crea y utiliza su propia variable local institucion
